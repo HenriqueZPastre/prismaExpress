6c189e778aba5f15435ce8809591eb989943bb90
Henrique <hpastre1@gmail.com>
2023-03-09 17:13:57 -0300

    changes

---
+++ prisma/migrations/20230309200518_init/migration.sql
@@ -0,0 +1,47 @@
+-- CreateTable
+CREATE TABLE "Contas" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "create_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletede_at" DATETIME,
+    "nome" TEXT NOT NULL,
+    "saldoInicial" REAL NOT NULL,
+    "saldoAtual" REAL NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Lancamentos" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "create_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletede_at" DATETIME,
+    "descricao" TEXT NOT NULL,
+    "valor" REAL NOT NULL,
+    "dataVencimento" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "contasId" INTEGER NOT NULL,
+    "dataPagamento" DATETIME,
+    CONSTRAINT "Lancamentos_contasId_fkey" FOREIGN KEY ("contasId") REFERENCES "Contas" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Tags" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "create_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletede_at" DATETIME,
+    "nome" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "lancamentos_tags" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "tagsId" INTEGER NOT NULL,
+    "lancamentosId" INTEGER NOT NULL,
+    CONSTRAINT "lancamentos_tags_tagsId_fkey" FOREIGN KEY ("tagsId") REFERENCES "Tags" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "lancamentos_tags_lancamentosId_fkey" FOREIGN KEY ("lancamentosId") REFERENCES "Lancamentos" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "contas_saldo" (
+    "contasId" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "saldoAtual" REAL NOT NULL,
+    "ultimoAtualizacao" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "contas_saldo_contasId_fkey" FOREIGN KEY ("contasId") REFERENCES "Contas" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
