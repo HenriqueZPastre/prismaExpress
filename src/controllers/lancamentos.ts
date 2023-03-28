f0ece8fa619902b80945ce619ed5e6fe29348b1a
Henrique <hpastre1@gmail.com>
2023-03-28 13:59:39 -0300

    adicionado paginator no listar lançamentos

--- src/controllers/lancamentos.ts
+++ src/controllers/lancamentos.ts
@@ -2,22 +2,62 @@ import { PrismaClient } from "@prisma/client"
 import { Request, Response } from "express"
 import { HandleResponse } from "../utils/HandleResponse"
 
-//Data de venciemnto é defaut now()
+
+/**
+ * 0 é despesa 1 é receita
+ * 
+ * Data de venciemnto é defaut now() em caso de estar em branco
+ */
 type Lancamentos = {
 	descricao: string,
 	valor: number,
 	dataVencimento: Date,
 	dataPagamento: Date,
+	/**
+	 * 0 é despesa 1 é receita
+	 */
 	tipo: 0 | 1,
 	contasId: number,
 	contasNome: string
 }
 
-const prism = new PrismaClient()
+let a: Lancamentos
+
+
+interface Query extends Request {
+	query: {
+		page?: string,
+		all?: string,
+		limit?: string
+	}
+}
+
+const prisma = new PrismaClient()
 
 export const LancamentosController = {
-	async listAll(req: Request, res: Response) {
-		const lancamentos = await prism.lancamentos.findMany({
+
+	/**
+	 * 
+	 * @param req 
+	 * @param res 
+	 * @returns 
+	 */
+	async listAll(req: Query, res: Response) {
+		let next
+		let limit: number | undefined = 15
+		if (req.query.all === 'true') {
+			next = undefined
+			limit = undefined
+		}
+
+		if (req.query.limit) { limit = Number(req.query.limit) }
+
+		const page = Number(req.query.page)
+		if (page > 1 && !req.query.all) {
+			next = (page - 1) * Number(limit)
+		}
+
+		const lancamentos = await prisma.lancamentos.findMany({
 			select: {
 				id: true,
 				descricao: true,
@@ -25,14 +65,20 @@ export const LancamentosController = {
 				dataVencimento: true,
 				dataPagamento: true,
 				tipo: true,
-				contasId: true
+				contasId: true,
+				contasNome: true
 			},
 			where: {
 				deletede_at: null
-			}
+			},
+			orderBy: {
+				id: 'desc',
+			},
+			skip: next,
+			take: limit,
 		})
 		if (lancamentos.length === 0) {
-			return HandleResponse(res, 404, "Nenhum lançamento encontrado")
+			return HandleResponse(res, 404, "Nenhum lançamento encontrado", 'Mensagem')
 		}
 		return HandleResponse(res, 200, lancamentos)
 	}
