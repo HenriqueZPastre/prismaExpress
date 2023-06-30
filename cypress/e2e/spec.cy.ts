78d1d2b02e690055fde84071f0e22d5cfc24ffae
Henrique <hpastre1@gmail.com>
2023-06-30 17:50:03 -0300

    refactor: ddd

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,53 +1,56 @@
 /// <reference types="cypress" />
-const url = '/tags'
+const url = '/tags?coluna=nomes'
 
 describe('Testes de GET na rota /tags', () => {
-	it('Sucesso', async () => {
+	it('Sucesso', () => {
 		cy.api({
 			method: 'GET',
 			url: url,
+			failOnStatusCode: false,
+			body:{}
 		}).then((response) => {
 			expect(response.status).be.equal(200)
-			expect(response.body.data).be.an('array').and.have.length(15)
-		})
-
-	})
-/* 
-	it('All true', async () => {
-		await r.get('?all=true').then((response) => {
-			expect(response.status).be.equal(200)
 			expect(response.body.data).be.an('array')
-			expect(response.body.data.length).toBeGreaterThan(15)
+			expect(response.body.data.length).greaterThan(15)
 		})
 	})
 
-	it('Take falha se all=true', async () => {
-		const take = 2
-		await r.get('?all=true&take=' + take).then((response) => {
-			expect(response.status).be.equal(200)
-			expect(response.body.data).be.an('array')
-			expect(response.body.data.length).not.eq(take)
-		})
-	})
-
-	it('Take sucess', async () => {
-		const take = 2
-		await r.get('?take=' + take).then((response) => {
-			expect(response.status).be.equal(200)
-			expect(response.body.data).be.an('array')
-			expect(response.body.data.length).eq(take)
-		})
-	})
-
-	it('stars', async () => {
-		await r
-			.post('')
-			.send({ 'nome': 555 })
-			.then((response) => {
-				console.log(JSON.stringify(response.body))
-				expect(response.status).be.equal(401)
+	/* 
+		it('All true', async () => {
+			await r.get('?all=true').then((response) => {
+				expect(response.status).be.equal(200)
+				expect(response.body.data).be.an('array')
+				expect(response.body.data.length).toBeGreaterThan(15)
 			})
-	}) */
+		})
+	
+		it('Take falha se all=true', async () => {
+			const take = 2
+			await r.get('?all=true&take=' + take).then((response) => {
+				expect(response.status).be.equal(200)
+				expect(response.body.data).be.an('array')
+				expect(response.body.data.length).not.eq(take)
+			})
+		})
+	
+		it('Take sucess', async () => {
+			const take = 2
+			await r.get('?take=' + take).then((response) => {
+				expect(response.status).be.equal(200)
+				expect(response.body.data).be.an('array')
+				expect(response.body.data.length).eq(take)
+			})
+		})
+	
+		it('stars', async () => {
+			await r
+				.post('')
+				.send({ 'nome': 555 })
+				.then((response) => {
+					console.log(JSON.stringify(response.body))
+					expect(response.status).be.equal(401)
+				})
+		}) */
 })
 
 
