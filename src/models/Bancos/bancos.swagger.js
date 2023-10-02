5378eea325a48264a57b8ba11cb6408fee87f157
Henrique <hpastre1@gmail.com>
2023-10-02 09:40:01 -0300

    refactor: dwdw

--- src/models/Bancos/bancos.swagger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ModelsSwaggerBancos = void 0;
-const zod_openapi_1 = require("@anatine/zod-openapi");
-const bancos_1 = require("./bancos");
-exports.ModelsSwaggerBancos = {
-    create: (0, zod_openapi_1.generateSchema)(bancos_1.zodBancos.create),
-    responseListarBancos: (0, zod_openapi_1.generateSchema)(bancos_1.zodBancos.listarBancos),
-};
