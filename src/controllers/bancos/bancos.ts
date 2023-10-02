650440454df794f0d46f4545dc91cda6880abf38
Henrique <hpastre1@gmail.com>
2023-10-02 09:06:25 -0300

    refactor: wdw

--- src/controllers/bancos/bancos.ts
+++ src/controllers/bancos/bancos.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 
 import { HandleResponse } from '../../utils/HandleResponse/HandleResponse'
 import { Response, Request } from 'express'
-import { ICreateBancos, IProcurarBancos, listarBancos } from '../../models/bancos/bancos.interface'
+import { ICreateBancos, IProcurarBancos, listarBancos } from '../../models/bancos/bancosInterface'
 
 const prisma = new PrismaClient()
 
