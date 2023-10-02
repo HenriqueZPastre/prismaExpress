bd0d3c119fc295c332abfae6b095b593ab02b13d
Henrique <hpastre1@gmail.com>
2023-10-02 11:42:05 -0300

    refactor: retorna o banco sem chamar nas rotas 2

--- src/controllers/bancos/bancos.ts
+++ src/controllers/bancos/bancos.ts
@@ -2,7 +2,7 @@ import { Response, Request } from 'express'
 import { HandleResponse } from '../../utils/HandleResponse/HandleResponse'
 import { PrismaClient } from '@prisma/client'
 
-import { ICreateBancos, IProcurarBancos, createBancos, listarBancos } from '@algo'
+import { ICreateBancos, IProcurarBancos, createBancos, listarBancos } from 'algo/bancoss.interface'
 
 const prisma = new PrismaClient()
 
